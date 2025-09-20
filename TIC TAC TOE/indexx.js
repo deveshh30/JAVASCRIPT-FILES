6d567da0302d66d1d765e7942eebbfd1c09cb4af
deveshh30 <devesh30gupta@gmail.com>
2025-09-21 00:19:51 +0530

    Initial commit

---
+++ TIC TAC TOE/indexx.js
@@ -0,0 +1,75 @@
+let boxes = document.querySelectorAll(".box");
+let resetbtn = document.querySelector(".reset");
+let msgContainer = document.querySelector(".msg-contain");
+let msg = document.querySelector("#msg");
+
+let turnA = true;
+
+const winpatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    console.log("box was clicked");
+    if (turnA) {
+      box.innerText = "O";
+      turnA = false;
+    } else {
+      box.innerText = "X";
+      turnA = true;
+    }
+    box.disabled = true;
+
+    checkWinner();
+  });
+});
+
+const disableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
+
+const enableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+};
+
+const showWinner = (winner) => {
+  msg.innerText = `Congratulations, winner is ${winner}`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+
+const checkWinner = () => {
+  for (let pattern of winpatterns) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+
+    if (pos1Val !== "" && pos2Val !== "" && pos3Val !== "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+        console.log("winner", pos1Val);
+        showWinner(pos1Val);
+      }
+    }
+  }
+};
+
+const resetGame = () => {
+  turnA = true;
+  enableBoxes();
+  msgContainer.classList.add("hide");
+};
+
+resetbtn.addEventListener("click", resetGame);
