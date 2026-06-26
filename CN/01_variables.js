6232b8c45f2725bc99e8cb77249e2162b6fba02a
deveshh30 <devesh30gupta@gmail.com>
2026-06-26 23:23:58 +0530

    datatype done

---
+++ CN/01_variables.js
@@ -0,0 +1,22 @@
+const name = "DEVESH GUPTA" ;
+let age = 18;
+var city = "DELHI";
+country = "INDIA";
+let continent ;
+
+age = 19;
+city = "banglore";
+country = "Germany"
+
+console.table([name , age ,city , country ,continent]);
+
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+
+Const is not changable at all
+var is function or global scoped
+let is block scoped
+
+*/
---
+++ CN/02_datatypes.js
@@ -0,0 +1,24 @@
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object 
+
+
+let age = 18;
+let age2 = "18";
+
+console.log(typeof(age));
+console.log(typeof(age2));
+
+let valinNum = Number(age);  // nno matter what the innput is it will changne into number type
+console.log(typeof(age));
