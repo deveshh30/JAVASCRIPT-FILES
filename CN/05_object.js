b4636b278b6380910f87aac2f58fa420b9fdf47f
deveshh30 <devesh30gupta@gmail.com>
2026-06-28 01:30:06 +0530

    object-pt1

---
+++ CN/05_object.js
@@ -0,0 +1,26 @@
+//object literals (singleton)
+
+const User = {
+    name : "devesh",
+    age : 18,
+    location : "delhi",
+    isLoggedIn : false,
+    lastLogin : ["Monday" , "Wednesday"]
+}
+
+console.log(User.name);
+// console.log(User[name]);  //this will not work we gotta tell engine its type to get a output
+console.log(User["name"]);
+
+
+User.name = "harshit"
+Object.freeze(User); //after this line no changes will be accepted
+User.name = "devesh"
+console.log(User);
+
+
+User.lastName = function() {
+    console.log("gupta");
+}
+
+console.log(User.lastName);
