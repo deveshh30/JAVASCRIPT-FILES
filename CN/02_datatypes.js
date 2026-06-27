8852ff1be50428d156766112533b9fa5a4a7d732
deveshh30 <devesh30gupta@gmail.com>
2026-06-27 21:05:02 +0530

    add data types and date handling examples

--- CN/02_datatypes.js
+++ CN/02_datatypes.js
@@ -22,3 +22,52 @@ console.log(typeof(age2));
 
 let valinNum = Number(age);  // nno matter what the innput is it will changne into number type
 console.log(typeof(age));
+
+
+//  Primitive = these datatypes value are immutable and stored directly into the memory
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive) = compared by value
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+/* Stack (primitive) -> value's copy is recieved, 
+heap(non-primitive) -> changes will occur in original value , value reference will be passed
+
+*/
+
+let myName = devesh;
+
+console.log(myName.charAt(0));
---
+++ CN/03_dates.js
@@ -0,0 +1,12 @@
+//dates 
+
+let myDate = new Date();
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleDateString());
+console.log(typeof myDate);
+
+let manualDate = new Date(2007 , 9  , 15);
+console.log(manualDate.toDateString());
+
